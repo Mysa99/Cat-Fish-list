dd7cd6768521baa578c0ea9e8e0f6bb88c66da30
Storm <78948929+Mysa99@users.noreply.github.com>
2021-03-21 19:38:47 +0100

    Yes this thing actually generates cat fish names. Updating in some days to get new one ^^

---
+++ Cat Fish List Name Generator.bat
@@ -0,0 +1,40 @@
+@echo off
+title Cat Fish Name Generator
+color 0a
+echo Welcome To The Official Cat Fish Name Generator by Storm.
+ping localhost -n 6 >nul
+cls
+echo If you wanna submit some weirdos and Cat Fishes, Submit it here
+echo https://forms.gle/X37cU62jvqqB5nA66
+ping localhost -n 5 >nul
+start https://forms.gle/X37cU62jvqqB5nA66
+start https://github.com/Mysa99/Cat-Fish-list
+ping localhost -n 5 >nul
+cls
+echo Generating Cat Fishes in one sec.
+ping localhost -n 5 >nul
+echo .Lara.
+echo Hana.
+echo Jana
+echo Maya
+echo Majanta
+echo The Ace#3119 (822965711558279208) 
+echo .cheesy#9999 (696695048238006344)
+echo !Choinek<3#1461 (789929113409945621)
+echo BitchDaFat#2464
+echo yJamLA
+echo KMJanaFKL
+echo MFGotThaRollie
+echo Paula
+echo Palestine
+echo Palma
+echo Pachia
+echo Paighton
+echo Maahi
+ping localhost -n 4 >nul
+echo Generated All Cat Fishes names, Saving them in 10 Seconds in a .txt file.
+ping localhost -n 10 >nul
+powershell -c $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://cdn.discordapp.com/attachments/817313797487853606/823262791691403264/Cat_Fish_List_21.03.2021.txt -OutFile "Cat_Fish_List_21.03.2021.txt"
+echo Succfully Downloaded The Cat Fish names in a .txt file, Have Fun exposing them :)
+pause
+
